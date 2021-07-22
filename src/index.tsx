0b0789a732cd74769aa1143bd9cd287bc9227fce
m.zhuravlev <maks723@yandex.ru>
2021-07-22 16:23:13 +0300

    add import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import {useEffect, useState} from 'react'
+import './styles.module.css'
 import styles from './styles.module.css'
+
 
 const defs = {
     nodeWidth: 400,
